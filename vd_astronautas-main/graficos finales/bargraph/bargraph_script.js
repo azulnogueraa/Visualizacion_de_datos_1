1414976d092a0032d2d6e477473c187d53384611
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-03-15 21:56:56 -0300

    Add files via upload

---
+++ vd_astronautas-main/graficos finales/bargraph/bargraph_script.js
@@ -0,0 +1,24 @@
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+  let chart = Plot.plot({
+    width:600,
+    height:300,
+    nice:true,
+    zero:true,
+    grid:true,
+    color: { Range:['pink', 'blue']},
+    marks: [
+      Plot.barY(data, {
+        x: 'nacionalidad',
+        y: 'mision_hs',
+        fill: 'genero',
+        sort: 'genero',
+        r: 5,
+        title: d => d.nacionalidad + '\n' + d.mision_hs,
+
+      }),
+    ],
+    marginLeft: 70,
+    width: 700,
+  })
+  d3.select('#chart').append(() => chart)
+})
