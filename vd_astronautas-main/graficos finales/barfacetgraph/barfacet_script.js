81b56a86b58f6e83351b494bbcf81ba82ad97a89
rochigonzalezc <94115157+rochigonzalezc@users.noreply.github.com>
2023-03-29 09:08:39 -0300

    upload bar facet graph

---
+++ vd_astronautas-main/graficos finales/barfacetgraph/barfacet_script.js
@@ -0,0 +1,38 @@
+let chart
+d3.csv('astronautas.csv', d3.autoType).then(data => {
+
+    const dataFiltered = data.filter(d => d.nacionalidad == "EE.UU." || d.nacionalidad == "Italia" || d.nacionalidad == "Japon" || d.nacionalidad == "U.S.S.R/Rusia")
+
+    chart = Plot.plot({
+        marks: [
+            Plot.barY(dataFiltered, {
+                x: 'ocupacion',
+                y: 'mision_hs',
+                fill: 'ocupacion',
+                fillOpacity: 1,
+                title: 'nacionalidad',
+              }),
+              Plot.frame(),
+            ],
+        
+            grid: true,
+            nice: true,
+            width: 1350,
+            height: 400,
+            facet: {
+              data: dataFiltered,
+              x: 'nacionalidad',
+            },
+        
+            color: {
+                legend: true,
+                className: 'legend-clusters',
+              },
+        
+            x: {axis: null},
+            y: {grid: true},
+            
+          })
+          d3.select('#chart').append(() => chart)
+        })
+        
