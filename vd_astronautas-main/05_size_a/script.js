c88b2970badef7477de8723a7e2f191dea0b092e
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-03 18:02:50 -0300

    Delete script.js

--- vd_astronautas-main/05_size_a/script.js
+++
@@ -1,9 +0,0 @@
-d3.csv("astronautas.csv", d3.autoType).then((data) => {
-  let chart = Plot.plot({
-    marks: [Plot.dot(data, { x: "anio_nacimiento", y: "edad_mision", r: "mision_hs" })],
-    nice: true,
-    line: true,
-    grid: true,
-  });
-  d3.select("#chart").append(() => chart);
-});
