e571c4f2251be50ed68b044543d6738b18f5768d
azulnogueraa <101016508+azulnogueraa@users.noreply.github.com>
2023-04-02 21:15:30 -0300

    Update agedot_script.js

--- vd_astronautas-main/graficos finales/agedotgraph/agedot_script.js
+++ vd_astronautas-main/graficos finales/agedotgraph/agedot_script.js
@@ -2,7 +2,7 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
   
     const colorScale = d3.scaleOrdinal()
     .domain(["masculino", "femenino"])
-    .range(["#03adfc", "#fc03d7"]);
+    .range(["#3399ff", "#ff0066"]);
 
     let chart = Plot.plot({
         marks: [
@@ -15,8 +15,10 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
             })),
             Plot.ruleY([0])
         ],
+
         width: 1200,
         height: 600,
+
         x: {
         label: "Edad →",
         nice: true,
@@ -25,6 +27,7 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
         tickSize: 0,
         ticks: 5,
         },
+
         y: {
         grid: true,
         nice: true,
@@ -34,6 +37,23 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
         tickSize: 0,
         ticks: 10,
         },
+
+        // color: {
+        //     range: ['#f97db0', '#4f96ff'],
+        //     legend: true,
+        // },
+
     })
     d3.select('#chart').append(() => chart)
+
+    // var fig = document.querySelector("figure")
+    // fig.childNodes[0].querySelectorAll("svg").forEach(s => {
+    //     s.style.borderRadius = "50%";
+    // })
+    // fig.childNodes[0].querySelectorAll("span").forEach(s => {
+    //     s.style.gap = "10px";
+    // })
+    // fig.childNodes[0].style.display = "flex";
+    // fig.childNodes[0].style.alignItems = "center";
+    // fig.childNodes[0].style.justifyContent = "center";
   })
